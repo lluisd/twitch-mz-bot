94bdf1888eacb3509a5669b6eac8f16d2796a16e
Lluis <lluisdanes@gmail.com>
2024-01-27 01:59:51 +0100

    port with express

--- app.js
+++ app.js
@@ -4,4 +4,11 @@ const messenger = new Messenger()
 messenger.listen()
     .then(() => {
         console.log('Connected')
+        const express = require('express')
+
+        const app = express();
+        var listener = app.listen(process.env.PORT, function() {
+            console.log('Listening on port ', + listener.address().port)
+            app.get('/', (req, res) => res.send('Hello World!'))
+        });
     })
