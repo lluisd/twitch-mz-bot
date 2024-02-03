89f9a1efefb809c1e8e84b23004c1d8736df7155
Lluis <lluisdanes@gmail.com>
2024-02-03 14:03:27 +0100

    edit stream title when is changed

--- handlers/stream.js
+++ handlers/stream.js
@@ -7,23 +7,39 @@ class Stream {
     async catchStream (bot) {
         const result = await TwitchService.getStream()
 
-        if (result && result.type === 'live') {
-            const image = `[\u200c](${result.thumbnail_url.replace('-{width}x{height}', '')})`
-            const link = `[${twitchUrl}${result.user_name}](${twitchUrl}${result.user_name})`
-            const directo = `*¡EN DIRECTO!*`
-            const text = `${image} ${directo}  ${link}`
+        if (result && result.type === 'live' ) {
+            const text = this._getText(result)
 
             const options = {
-                parse_mode: 'markdown'
+                parse_mode: 'Markdown'
             }
 
             await bot.sendMessage(config.telegram.chatId, text, options).then((msg) => {
                      TwitchService.saveLastMessage(msg)
+                     TwitchService.saveTitle(result.title)
                 })
         } else if (result && result.type === 'finished' && result.messageId) {
             await bot.deleteMessage(config.telegram.chatId, result.messageId)
             await TwitchService.deleteLastMessage()
+            await TwitchService.saveTitle(result.title)
+        } else if (result && result.type === 'stillLive' && result.messageId && result.lastTitle !== result.title) {
+            const options = {
+                chat_id: config.telegram.chatId,
+                message_id: result.messageId,
+                parse_mode: 'Markdown'
+            }
+            try {
+                await bot.editMessageText(this._getText(result), options)
+            } catch {}
+            await TwitchService.saveTitle(result.title)
         }
+    }
+
+    _getText (stream) {
+        const image = `[\u200c](${stream.thumbnail_url.replace('-{width}x{height}', '')})`
+        const link = `[${twitchUrl}${stream.user_name}](${twitchUrl}${stream.user_name})`
+        const title = `🔴 *¡EN DIRECTO!*`
+        return `${image} ${title}  ${link} \n _${stream.title}_`
     }
 }
 
