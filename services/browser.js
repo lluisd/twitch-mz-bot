15ea1b5fb302217a26caa40f050cc5c3a3e00321
Lluis <lluisdanes@gmail.com>
2024-02-05 23:12:18 +0100

    singleton borwser api

--- services/browser.js
+++ services/browser.js
@@ -1,27 +1,17 @@
-const config = require('../config')
-const puppeteer = require("puppeteer-core")
+const browserApi = require('./browser-api.js')
 require('mathjs')
 
 async function getScreenshot() {
     try {
-        const browser = await puppeteer.connect({ browserWSEndpoint: `wss://${config.browserlessUrl}` })
-        const page = await browser.newPage()
+        const page = await browserApi.getPage()
 
-        await page.setViewport({ width: 1920, height: 1080 })
-        await page.goto("https://www.twitch.tv/" + config.twitch.channels, { waitUntil: 'networkidle0' })
 
-        await page.$eval('button[data-a-target="consent-banner-accept"]', el =>  el.click()).catch(() => {})
-        await page.$eval('button[data-a-target="content-classification-gate-overlay-start-watching-button"]', el =>  el.click()).catch(() => {})
 
-        await page.waitForSelector('div.persistent-player')
-        await page.$eval('.video-player__default-player', el => el.remove())
-        const svgImage = await page.$('div.persistent-player')
         const name = Math.random().toString(36).substring(2,8)
-        const bufferImage = await svgImage.screenshot({
+        const bufferImage = await browserApi.getSvgImage().screenshot({
             path: `public/images/${name}.png`,
             omitBackground: true
         })
-        await browser.close()
 
         return {buffer: bufferImage, fileName: `${name}.png` }
     } catch (error) {
