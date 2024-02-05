6628f6d17aae22f1248879dbbca1bed2783a4e73
Lluis <lluisdanes@gmail.com>
2024-02-05 11:58:24 +0100

    disable TG notifications

--- handlers/stream.js
+++ handlers/stream.js
@@ -13,11 +13,11 @@ class Stream {
         const image = await BrowserService.getScreenshot()
         if (image) {
             await bot.say(target, `Captura de ${user}: ${config.externalUrl}/images/${image.fileName}`)
-            const channel = await TwitchService.getChannel()
-            await notifierBot.sendPhoto(config.telegram.chatId, image.buffer, {
-                caption: `Captura del directo _${channel.title}_ \n por *${user}*`,
-                parse_mode: 'Markdown'
-            })
+            // const channel = await TwitchService.getChannel()
+            // await notifierBot.sendPhoto(config.telegram.chatId, image.buffer, {
+            //     caption: `Captura del directo _${channel.title}_ \n por *${user}*`,
+            //     parse_mode: 'Markdown'
+            // })
         }
     }
 
