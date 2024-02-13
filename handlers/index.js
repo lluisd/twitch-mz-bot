e1361e4ba12f8d2067a4de7fd260b444515e0544
Lluis <lluisdanes@gmail.com>
2024-02-14 00:30:40 +0100

    Add birthday feature

--- handlers/index.js
+++ handlers/index.js
@@ -2,10 +2,12 @@ const Generic = require('./generic')
 const Weather = require('./Weather')
 const Train = require('./Train')
 const Stream = require('./Stream')
+const Birthday = require('./Birthday')
 
 module.exports = {
     generic: new Generic(),
     weather: new Weather(),
     train: new Train(),
-    stream: new Stream()
+    stream: new Stream(),
+    birthday: new Birthday()
 }
