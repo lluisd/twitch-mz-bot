12190c69cd14f7cc9256a0e207d2aa6d37cc74c6
Lluis <lluisdanes@gmail.com>
2024-04-04 15:45:11 +0200

    Remove setviewport

--- helpers/browserApi.js
+++ helpers/browserApi.js
@@ -22,11 +22,11 @@ class PuppeteerApi {
     }
 
     async handleStart() {
-        await this.page.setViewport({ width: 1920, height: 1080 })
+        //await this.page.setViewport({ width: 1920, height: 1080 })
         await this.page.goto("https://www.twitch.tv/" + config.twitch.channels, { waitUntil: ['networkidle0',  'domcontentloaded'] })
-        const fullPage = await this.page.$('body')
-        const fullPageSize = await fullPage.boundingBox()
-        await this.page.setViewport({ width: 1920, height: parseInt(fullPageSize.height) })
+        //const fullPage = await this.page.$('body')
+        //const fullPageSize = await fullPage.boundingBox()
+        //await this.page.setViewport({ width: 1920, height: parseInt(fullPageSize.height) })
         await this.removeElementsAndGetDiv()
     }
 
