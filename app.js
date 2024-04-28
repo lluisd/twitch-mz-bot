0d76728487ab577bb1ece1cd49c2ec08be44199b
Lluis <lluisdanes@gmail.com>
2024-04-28 21:22:29 +0200

    add random links

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Messenger = require('./lib/messenger')
 const mongoose = require('mongoose')
 const config = require('./config')
 const express = require("express")
+const randomLinks = require("./config/randomLinks.json");
 
 mongoose.connect(config.database).then(() => {
     const messenger = new Messenger()
@@ -17,6 +18,12 @@ mongoose.connect(config.database).then(() => {
             app.get('/i/:id', (req, res) => {
                 res.sendFile(__dirname + `/public/images/${req.params.id}.jpg`)
             });
+
+            app.get('/OF/:id', (req, res) => {
+                const position = parseInt(req.params.id.replace(config.twitch.channels, ''))
+                const link = randomLinks.links[position]
+                res.redirect(link.link);
+            })
 
             const listener = app.listen(process.env.PORT, ()=>  {
                 console.log('Listening on port ', + listener.address().port)
