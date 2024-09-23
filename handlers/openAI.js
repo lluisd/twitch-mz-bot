f6dcc9eed4a680df772ea5b464c0600f6b2ade96
Lluis <lluisdanes@gmail.com>
2024-09-23 23:02:37 +0200

    openai by using rest calls

--- handlers/openAI.js
+++ handlers/openAI.js
@@ -1,9 +1,11 @@
 const OpenAIService = require('../services/openAI')
 
 class OpenAI {
-    async askOpenAI (target, text, bot) {
+    async askOpenAI (target, text, username, bot) {
         const response  = await OpenAIService.askOpenAI(text)
-        bot.say(target, `${response}`)
+        if (response) {
+            bot.say(target, `@${username} ${response}`)
+        }
     }
 }
 
