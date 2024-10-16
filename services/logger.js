69a45da7e2c27e2cd225a0ac595c9732929c95f2
Lluis <lluisdanes@gmail.com>
2024-10-16 15:49:50 +0200

    undo changes

--- services/logger.js
+++ services/logger.js
@@ -1,20 +1,15 @@
 const dbManager = require('../helpers/dbmanager')
 const moment = require('moment')
-const config = require('../config')
 
 async function logChatMessage(roomId, username, text) {
     let result = null
-    if (config.openAI.logging) {
-        result = await dbManager.addChatLogLine(roomId, username, text, moment())
-    }
+    result = await dbManager.addChatLogLine(roomId, username, text, moment())
     return result
 }
 
 async function logStreamTitle(roomId, title) {
     let result = null
-    if (config.openAI.logging) {
-        result = await dbManager.addTitleLogLine(roomId, title, moment())
-    }
+    result = await dbManager.addTitleLogLine(roomId, title, moment())
     return result
 }
 
