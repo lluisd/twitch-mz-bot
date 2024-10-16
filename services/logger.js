b276b00549472d9a9ebe5ed391c6ab0ca86f45d7
Lluis <lluisdanes@gmail.com>
2024-10-16 12:15:42 +0200

    event sub

--- services/logger.js
+++ services/logger.js
@@ -1,15 +1,20 @@
 const dbManager = require('../helpers/dbmanager')
 const moment = require('moment')
+const config = require('../config')
 
 async function logChatMessage(roomId, username, text) {
     let result = null
-    result = await dbManager.addChatLogLine(roomId, username, text, moment())
+    if (config.openAI.logging) {
+        result = await dbManager.addChatLogLine(roomId, username, text, moment())
+    }
     return result
 }
 
 async function logStreamTitle(roomId, title) {
     let result = null
-    result = await dbManager.addTitleLogLine(roomId, title, moment())
+    if (config.openAI.logging) {
+        result = await dbManager.addTitleLogLine(roomId, title, moment())
+    }
     return result
 }
 
