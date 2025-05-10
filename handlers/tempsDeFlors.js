970970ad58dcfe9b2c1a13ae0b4b199797a2e6cb
Lluis Danes Pages <Lluis.Danes@sunwebgroup.com>
2025-05-10 18:44:38 +0200

    deactivate on set visited

--- handlers/tempsDeFlors.js
+++ handlers/tempsDeFlors.js
@@ -41,6 +41,7 @@ class TempsDeFlors {
         const spotNumber = parseInt(text)
         if (!isNaN(spotNumber)) {
             const spot = await TempsDeFlorsService.setTFVisited(roomId, spotNumber)
+            await TwitchService.setActiveSpot(0)
             if (spot) {
                 await this._printSpot(spot, target, bot)
             }
