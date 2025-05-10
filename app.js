ef2c824f6217bca07b453690d5a1180607be9721
Lluis Danes Pages <Lluis.Danes@sunwebgroup.com>
2025-05-10 13:54:03 +0200

    enable temps de flors

--- app.js
+++ app.js
@@ -33,153 +33,207 @@ mongoose.connect(config.database).then(() => {
                 next();
             });
 
-            app.get('/transcribe', async function(req, res) {
-                await HAService.hibernateTranscriberPC()
-                await handlers.openAI.uploadStreamToOpenai(`#${config.twitch.channels}`, twitchBot, telegramBot)
-                const response = {
-                    message: 'transcription started',
-                    status: 'success'
-                };
-                res.json(response);
-            });
+            app.use((err, req, res, next) => {
+                logger.error(err.stack)
+                res.status(500).json({ message: 'Internal Server Error' })
+            })
 
-            app.get('/p/:id', (req, res) => {
-                const spotNumber = parseInt(req.params.id)
-                if (typeof spotNumber === 'number') {
-                    TempsDeFlorsService.getTFSpot(config.twitch.roomId, spotNumber).then((spot) => {
-                        res.redirect(`https://www.google.com/maps?q=${spot.coordinates}`)
-                    })
+            app.get('/transcribe', async function(req, res, next) {
+                try{
+                    await HAService.hibernateTranscriberPC()
+                    await handlers.openAI.uploadStreamToOpenai(`#${config.twitch.channels}`, twitchBot, telegramBot)
+                    const response = {
+                        message: 'transcription started',
+                        status: 'success'
+                    };
+                    res.json(response);
+                } catch (error) {
+                    next(error)
                 }
             });
 
-            app.get('/listado', function(req, res) {
-                TempsDeFlorsService.getTFSpots(config.twitch.roomId).then((spots) => {
-                    const percentage = parseFloat((spots.filter((s) => s.visited).length / spots.length * 100).toFixed(2))
-                    res.render('pages/list',{
-                        spots: spots,
-                        url: config.externalUrl,
-                        channel: config.twitch.channels,
-                        percentage: percentage,
-                        moment: moment,
-                        bgClass: percentage > 50 ? 'bg-success' : (percentage > 25 ? 'bg-warning' : 'bg-danger')
-                    });
-                })
+            app.get('/p/:id', (req, res, next) => {
+                try{
+                    const spotNumber = parseInt(req.params.id)
+
+                    if (typeof spotNumber === 'number') {
+                        TempsDeFlorsService.getTFSpot(config.twitch.roomId, spotNumber).then((spot) => {
+                            res.redirect(`https://www.google.com/maps?q=${spot.coordinates}`)
+                        })
+                    }
+                } catch (error) {
+                    next(error)
+                }
             });
 
-            app.get('/fotos', function(req, res) {
-                TempsDeFlorsService.getTFSpots(config.twitch.roomId).then((spots) => {
-                    res.render('pages/gallery',{
-                        spots: spots.filter((s) => s.screenshot !== null),
-                        url: config.externalUrl,
-                        channel: config.twitch.channels
-                    });
-                })
-            });
-
-            app.get('/stream', function(req, res) {
-                TwitchService.getChannel().then(async (channel) => {
-                    if (channel) {
-                        const screenshots = await ScreenshotService.getScreenshots(channel.streamId)
-                        res.render('pages/stream',{
-                            screenshots: screenshots,
+            app.get('/reto', function(req, res, next) {
+                try {
+                    TempsDeFlorsService.getTFSpots(config.twitch.roomId).then((spots) => {
+                        const percentage = parseFloat((spots.filter((s) => s.visited).length / spots.length * 100).toFixed(2))
+                        res.render('pages/list',{
+                            spots: spots,
                             url: config.externalUrl,
                             channel: config.twitch.channels,
+                            percentage: percentage,
                             moment: moment,
-                            title: channel.title
+                            bgClass: percentage > 50 ? 'bg-success' : (percentage > 25 ? 'bg-warning' : 'bg-danger')
                         });
-                    }
-                });
+                    })
+                } catch (error) {
+                    next(error)
+                }
             });
 
-            app.get('/comandos', function(req, res) {
-                res.render('pages/comandos',{
-                    channel: config.twitch.channels
-                });
+            app.get('/fotos', function(req, res, next) {
+                try{
+                    TempsDeFlorsService.getTFSpots(config.twitch.roomId).then((spots) => {
+                        res.render('pages/gallery',{
+                            spots: spots.filter((s) => s.screenshot !== null),
+                            url: config.externalUrl,
+                            channel: config.twitch.channels
+                        });
+                    })
+                } catch (error) {
+                    next(error)
+                }
             });
 
-            app.get('/rutas', function(req, res) {
-                res.render('pages/rutas',{
-                    channel: config.twitch.channels
-                });
+            app.get('/stream', function(req, res, next) {
+                try {
+                    TwitchService.getChannel().then(async (channel) => {
+                        if (channel) {
+                            const screenshots = await ScreenshotService.getScreenshots(channel.streamId)
+                            res.render('pages/stream',{
+                                screenshots: screenshots,
+                                url: config.externalUrl,
+                                channel: config.twitch.channels,
+                                moment: moment,
+                                title: channel.title
+                            });
+                        }
+                    });
+                } catch (error) {
+                    next(error)
+                }
             });
 
-            app.get('/bans', function(req, res) {
-                TwitchService.getBannedUsersCountByDate(moment().subtract(10, 'years').startOf('year').toDate()).then((bans) => {
-                    res.render('pages/bans',{
-                        bans:  bans.filter(ban => config.blacklistUsers.indexOf(ban.userId.toString()) === -1)
-                            .map(e => {
-                                const now = moment().tz('Europe/Madrid');
-                                const creationDate = moment(e.creationDate);
-                                const duration = moment.duration(now.diff(creationDate));
-                                return {
-                                    ...e,
-                                    days: Math.floor(duration.asDays()),
-                                    hours: duration.hours(),
-                                    minutes: duration.minutes()
-                                }
-                            })
-                            .reverse(),
-                        url: config.externalUrl,
+            app.get('/comandos', function(req, res, next) {
+                try {
+                    res.render('pages/comandos',{
                         channel: config.twitch.channels
                     });
-                });
+                } catch (error) {
+                    next(error)
+                }
             });
 
-            app.get('/timeouts', function(req, res) {
-                TwitchService.getTimeouts().then((timeouts) => {
-                    res.render('pages/timeouts',{
-                        timeouts:  timeouts
-                            .map(e => {
-                                const now = moment().tz('Europe/Madrid');
-                                const expiryMoment = moment(e.expiryDate);
-                                const duration = moment.duration(expiryMoment.diff(now));
-                                return {
-                                    ...e,
-                                    days: duration.days(),
-                                    hours: duration.hours(),
-                                    minutes: duration.minutes(),
-                                    seconds: duration.seconds()
-                                }
-                            })
-                            .reverse(),
-                        url: config.externalUrl,
+            app.get('/rutas', function(req, res, next) {
+                try {
+                    res.render('pages/rutas',{
                         channel: config.twitch.channels
                     });
-                });
+                } catch (error) {
+                    next(error)
+                }
+            });
+
+            app.get('/bans', function(req, res, next) {
+                try {
+                    TwitchService.getBannedUsersCountByDate(moment().subtract(10, 'years').startOf('year').toDate()).then((bans) => {
+                        res.render('pages/bans',{
+                            bans:  bans.filter(ban => config.blacklistUsers.indexOf(ban.userId.toString()) === -1)
+                                .map(e => {
+                                    const now = moment().tz('Europe/Madrid');
+                                    const creationDate = moment(e.creationDate);
+                                    const duration = moment.duration(now.diff(creationDate));
+                                    return {
+                                        ...e,
+                                        days: Math.floor(duration.asDays()),
+                                        hours: duration.hours(),
+                                        minutes: duration.minutes()
+                                    }
+                                })
+                                .reverse(),
+                            url: config.externalUrl,
+                            channel: config.twitch.channels
+                        });
+                    });
+                } catch (error) {
+                    next(error)
+                }
+            });
+
+            app.get('/timeouts', function(req, res, next) {
+                try {
+                    TwitchService.getTimeouts().then((timeouts) => {
+                        res.render('pages/timeouts',{
+                            timeouts:  timeouts
+                                .map(e => {
+                                    const now = moment().tz('Europe/Madrid');
+                                    const expiryMoment = moment(e.expiryDate);
+                                    const duration = moment.duration(expiryMoment.diff(now));
+                                    return {
+                                        ...e,
+                                        days: duration.days(),
+                                        hours: duration.hours(),
+                                        minutes: duration.minutes(),
+                                        seconds: duration.seconds()
+                                    }
+                                })
+                                .reverse(),
+                            url: config.externalUrl,
+                            channel: config.twitch.channels
+                        });
+                    });
+                } catch (error) {
+                    next(error)
+                }
             });
 
             app.use('/images', express.static('images'));
 
-            app.get('/i/:id',  async(req, res) => {
-                const { id } = req.params;
-                TwitchService.getChannel().then((channel) => {
-                    if (channel) {
-                        ScreenshotService.getScreenshots(channel.streamId).then ((screenshots) => {
-                            if (screenshots.length > 0) {
-                                const image = screenshots.find((s) => s.name === id)
-                                if (image) {
-                                    res.redirect(config.externalUrl + `/stream/#lg=1&slide=${id}`);
+            app.get('/i/:id',  async(req, res , next) => {
+                try {
+                    const { id } = req.params;
+                    TwitchService.getChannel().then((channel) => {
+                        if (channel) {
+                            ScreenshotService.getScreenshots(channel.streamId).then ((screenshots) => {
+                                if (screenshots.length > 0) {
+                                    const image = screenshots.find((s) => s.name === id)
+                                    if (image) {
+                                        res.redirect(config.externalUrl + `/stream/#lg=1&slide=${id}`);
+                                    } else {
+                                        res.sendFile(__dirname + `/public/images/${id}.jpg`)
+                                    }
                                 } else {
                                     res.sendFile(__dirname + `/public/images/${id}.jpg`)
                                 }
-                            } else {
-                                res.sendFile(__dirname + `/public/images/${id}.jpg`)
-                            }
-                        });
-                    } else {
-                        res.sendFile(__dirname + `/public/images/${id}.jpg`)
-                    }
-                });
+                            });
+                        } else {
+                            res.sendFile(__dirname + `/public/images/${id}.jpg`)
+                        }
+                    });
+                } catch (error) {
+                    next(error)
+                }
             });
 
-            app.get('/img/:id', (req, res) => {
-                res.sendFile(__dirname + `/public/images/${req.params.id}.jpg`)
+            app.get('/img/:id', (req, res, next) => {
+                try {
+                    res.sendFile(__dirname + `/public/images/${req.params.id}.jpg`)
+                } catch (error) {
+                    next(error)
+                }
             });
 
-            app.get('/OF/:id', (req, res) => {
-                const position = parseInt(req.params.id.replace(config.twitch.channels, ''))
-                const link = randomLinks.links[position]
-                res.redirect(link.link);
+            app.get('/OF/:id', (req, res, next) => {
+                try {
+                    const position = parseInt(req.params.id.replace(config.twitch.channels, ''))
+                    const link = randomLinks.links[position]
+                    res.redirect(link.link);
+                } catch (error) {
+                    next(error)
+                }
             })
 
             const eventSub = new EventSub();
